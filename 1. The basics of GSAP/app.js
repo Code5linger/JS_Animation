9ce46f639ba9faf1ff41cc78debfd25b0f7fe2d6
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-17 16:53:26 +0600

    Simple Rotating Element

---
+++ 1. The basics of GSAP/app.js
@@ -0,0 +1,12 @@
+const img1 = document.querySelector(".img1");
+/*
+const index = 1;
+
+gsap.to(`.img${index}`, {
+  x: 100,
+  y: 100,
+  rotation: "45deg",
+  backgroundColor: "#fff",
+  duration: 1,
+});
+*/
