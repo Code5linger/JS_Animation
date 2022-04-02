272618cefd81186e85d064dcc4b2fb90aba1f988
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-02 21:46:13 +0600

    W

--- U-TheCompleteGSAP/05 GSAP Web Animations/script.js
+++ U-TheCompleteGSAP/05 GSAP Web Animations/script.js
@@ -109,5 +109,26 @@ gsap.from(".circle", {
   y: "random(-200,200)",
   stagger: 0.25,
 });
-
 */
+let tl = gsap.timeline({ repeat: 2 });
+
+tl.from(".logo", {
+  duration: 1.5,
+  opacity: 0,
+  scale: 0.3,
+  ease: "back",
+});
+
+tl.to(".logo", { duration: 1, rotation: 360 });
+
+tl.from(
+  ".circle",
+  {
+    duration: 1,
+    opacity: 0,
+    delay: 1.5,
+    y: 150,
+    stagger: 0.25,
+  },
+  "+=1"
+);
