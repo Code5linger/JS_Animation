19428a191599a79025447a4312307bcf56ed9df9
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-03 20:59:52 +0600

    W

--- U-TheCompleteGSAP/05 GSAP Web Animations/Besic Animations/script.js
+++ U-TheCompleteGSAP/05 GSAP Web Animations/Besic Animations/script.js
@@ -1,5 +1,19 @@
+
 TweenMax.to(".circle", 2, {
   backgroundColor: "red",
   x: 200,
   ease: Elastic.easeOut,
+});
+
+
+TweenMax.to(".circle", 2, {
+  backgroundColor: "red",
+  x: 200,
+  ease: Elastic.easeIn,
+});
+
+TweenMax.to(".circle", 2, {
+  backgroundColor: "red",
+  x: 200,
+  ease: Elastic.easeInOut,
 });
