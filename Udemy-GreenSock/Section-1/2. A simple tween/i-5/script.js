d11e593a69e77798d04af3cb540ffcfc6b248da8
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-04 18:14:11 +0600

    i-5

--- Udemy-GreenSock/Section-1/2. A simple tween/i-5/script.js
+++ Udemy-GreenSock/Section-1/2. A simple tween/i-5/script.js
@@ -1,10 +1,10 @@
-TweenMax.to(".circle", 1, {
+TweenMax.to($(".circle"), 1, {
   x: 200,
   y: 200,
   backgroundColor: "silver",
 });
 
-TweenMax.to(".square", 2, {
+TweenMax.to($(".square"), 2, {
   x: -200,
   y: -200,
   scale: 2,
@@ -13,8 +13,32 @@ TweenMax.to(".square", 2, {
   ease: Elastic.easeOut,
 });
 
-TweenMax.from("#rectangle", 2, {
+TweenMax.from($("#rectangle"), 2, {
   y: 200,
   rotate: 720,
   scale: 4,
+});
+
+TweenMax.to($("li"), 3, {
+  x: 10,
+});
+
+TweenMax.to($("li:first-child"), 1, {
+  x: -100,
+});
+
+TweenMax.to($("li:last-child"), 1, {
+  x: 100,
+});
+
+TweenMax.to($("li:nth-child(2)"), 1, {
+  x: 22,
+});
+
+TweenMax.to($("li:nth-child(odd)"), 1, {
+  x: -50,
+});
+
+TweenMax.to($("li:nth-child(even)"), 1, {
+  x: 50,
 });
