edac1516600a6f81796d873f4b8e8c57d27a5c60
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-04 15:40:51 +0600

    i-6

---
+++ Udemy-GreenSock/Section-1/2. A simple tween/i-6/script.js
@@ -0,0 +1,13 @@
+TweenMax.to(".circle", 1, {
+  x: 200,
+  y: 200,
+  backgroundColor: "silver",
+});
+
+TweenMax.to(".square", 2, {
+  x: -200,
+  y: -200,
+  backgroundColor: "black",
+  ease: Back.easeOut,
+  ease: Elastic.easeOut,
+});
