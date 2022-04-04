9cd25057e85dd7249d3af662b96dbf271a40ec52
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-04 19:59:13 +0600

    Tla-1

---
+++ Udemy-GreenSock/Section-1/Time Line Animation/TLA-1/js/main.js
@@ -0,0 +1,13 @@
+let timeLineAnimation = TweenMax.to(".circle", 1, {
+  x: 100,
+});
+
+TweenMax.to(".square", 1, {
+  x: 100,
+  delay: 1,
+});
+
+TweenMax.to(".rectangle", 1, {
+  x: 100,
+  delay: 2,
+});
