e1f6ba4d8099adfa4ed1046b8a3a4618494628ea
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-04 15:19:52 +0600

    i-4

---
+++ Udemy-GreenSock/Section-1/2. A simple tween/i-4/script.js
@@ -0,0 +1,15 @@
+TweenMax.to(".circle", 1, {
+  x: 100,
+  y: 100,
+  backgroundColor: "silver",
+});
+
+TweenMax.to(".square", 2, {
+  x: -200,
+  y: -200,
+  backgroundColor: "red",
+  scale: 2,
+  delay: 1,
+  //ease: Back.easeOut,
+  ease: Elastic.easeOut,
+});
