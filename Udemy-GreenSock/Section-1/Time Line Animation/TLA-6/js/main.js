1267f556ef71620e58012ded10674438fef270f8
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-04 22:00:39 +0600

    TLA-6

--- Udemy-GreenSock/Section-1/Time Line Animation/TLA-6/js/main.js
+++ Udemy-GreenSock/Section-1/Time Line Animation/TLA-6/js/main.js
@@ -11,7 +11,7 @@ TweenMax.to(".rectangle", 1, {
   x: 100,
 });
 */
-
+/*
 let tl = new TimelineMax();
 
 tl.to(".circle", 1, {
@@ -23,3 +23,16 @@ tl.to(".circle", 1, {
   .to(".rectangle", 1, {
     x: 100,
   });
+*/
+
+TweenMax.staggerFrom(
+  ".triangle",
+  1,
+  {
+    x: 200,
+    y: 200,
+    Opacity: 0,
+    ease: Back.eseOut,
+  },
+  0.1
+);
