8422b3a7c18dc214790292e0018ff97f429e9f86
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-05 14:42:45 +0600

    PC

--- Udemy-GreenSock/Section-1/Time Line Animation/Genesis/js/main.js
+++ Udemy-GreenSock/Section-1/Time Line Animation/Genesis/js/main.js
@@ -31,7 +31,7 @@ timeLineAnimation
     ease: Sine.easeIn,
   });
 */
-
+/*
 TweenMax.staggerFrom(
   ".triangle",
   1,
@@ -42,6 +42,7 @@ TweenMax.staggerFrom(
   },
   0.2
 );
+*/
 /*
 TweenMax.set(".oval", {
   x: -100,
@@ -74,3 +75,39 @@ tl.to(".circle", 1, { x: 100 })
   .to(".rectangle", 1, { x: 100 }, 5);
 
 */
+
+let tl = new TimelineMax();
+
+tl.staggerFrom(".shape", 0.5, { x: 50, y: 50, opacity: 0 }, 0.2);
+
+$(".playButton").click(function () {
+  tl.play();
+});
+
+$(".pauseButton").click(function () {
+  tl.pause();
+});
+
+$(".resumeButton").click(function () {
+  tl.resume();
+});
+
+$(".reverseButton").click(function () {
+  tl.reverse();
+});
+
+$(".slowButton").click(function () {
+  tl.timeScale(0.5);
+});
+
+$(".fastButton").click(function () {
+  tl.timeScale(5);
+});
+
+$(".seekButton").click(function () {
+  tl.seek(1.5);
+});
+
+$(".progressButton").click(function () {
+  tl.progress(0.7);
+});
