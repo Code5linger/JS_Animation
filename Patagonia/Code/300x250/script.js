46d79dbd9e59c37d927b824a7b100f6f9b8ce8e1
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-06 15:22:58 +0600

    ??

---
+++ Patagonia/Code/300x250/script.js
@@ -0,0 +1,37 @@
+//TweenMax.set(".imgOne", { x: -100 });
+/*
+let tl = new TimelineMax({ repeat: -1 });
+
+tl.to(".imgOne", 1, { x: 100 }).to(".imagOne", 1, { x: -100 });
+*/
+/*
+TweenMax.set(".imgOne", {
+  x: -100,
+});
+
+let tl = new TimelineMax({ repeat: -1 });
+
+tl.to(".imgOne", 2, {
+  x: 100,
+});
+*/
+/*
+TweenMax.set(".imgOne", {
+  x: 0,
+});
+
+let tl = new TimelineMax({ repeat: -1, repeatDelay: 0 });
+
+tl.to(".imgOne", 1, {
+  x: -600,
+})
+  .to(".imgTwo", 1, {
+    x: -600,
+  })
+  .to(".imgTwo", 1, {
+    x: -1200,
+  })
+  .to(".imgThree", 1, {
+    x: -1800,
+  });
+*/
