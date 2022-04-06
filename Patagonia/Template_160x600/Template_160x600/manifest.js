b563174c7fb5af9be23a1bb84d024668970aaf0b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-06 14:14:21 +0600

    1

---
+++ Patagonia/Template_160x600/Template_160x600/manifest.js
@@ -0,0 +1,12 @@
+FT.manifest({
+	"filename": "index.html",
+	"width": 160,
+	"height": 600,
+	"clickTagCount": 1,
+	"instantAds":[
+	    {"name": "dynamicText",  "type": "text",  "default": "Join us on the mountain"},
+	    {"name": "dynamicCTA",  "type": "text",  "default": "Buy Tickets"},
+	    {"name": "dynamicImage", "type": "image", "default": "default_160x600.jpg"},
+	    {"name": "dynamicClicktag", "type": "text", "default": "https://www.flashtalking.com"}
+	]
+});
