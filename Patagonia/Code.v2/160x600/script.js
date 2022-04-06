cfee76776ae36089fae0f45da704a579f40e8ed8
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-06 22:21:12 +0600

    160x600

--- Patagonia/Code.v2/160x600/script.js
+++ Patagonia/Code.v2/160x600/script.js
@@ -32,10 +32,10 @@ tl.to(".img1", 1, {
   })
   */
 tl.fromTo(".img1", { x: 0, delay: 5 }, { x: 0 }, "100%");
-tl.fromTo(".img1", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img2", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img2", { x: -300 }, { x: -300, delay: 5 }, "100%");
+tl.fromTo(".img1", { x: 0 }, { x: -160 }, "100%");
+tl.fromTo(".img2", { x: 0 }, { x: -160 }, "100%");
+tl.fromTo(".img2", { x: -300 }, { x: -160, delay: 5 }, "100%");
 
-tl.fromTo(".img2", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img3", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img3", { x: 0 }, { x: -600, delay: 6 }, "100%");
+tl.fromTo(".img2", { x: 0 }, { x: -160 }, "100%");
+tl.fromTo(".img3", { x: 0 }, { x: -160 }, "100%");
+tl.fromTo(".img3", { x: 0 }, { x: -320, delay: 6 }, "100%");
