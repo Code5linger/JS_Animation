423debb69815e3268c8988ae7ada419d43fb45d4
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-08 20:49:37 +0600

    W

--- Patagonia/CodeV3/300x600/script.js
+++ Patagonia/CodeV3/300x600/script.js
@@ -1,42 +1,26 @@
-let tl = new TimelineMax({ delay: 3, repeat: -1 });
-/*
-tl.to(".img1", 1, {
-  x: -600,
-  delay: 2,
-  opacity: 0,
-})
-  .to(".img2", 1, {
-    x: -600,
-    delay: -1,
-  })
-  .to(".img2", 1, {
-    x: -1200,
-    delay: 2,
-  })
-  .to(".img3", 1, {
-    x: -600,
-    delay: -1,
-  })
-  .to(".img3", 1, {
-    x: -1200,
-    delay: 0,
-  });
-/*
-  .to(".img3", 1, {
-    x: -600,
-    delay: -1,
-  })
-  .to(".img3", 1, {
-    x: -1200,
-    delay: 1,
-  })
-  */
+let tl = new TimelineMax({ repeat: -1 });
 
-tl.fromTo(".img1", { x: 0, delay: 5 }, { x: 0 }, "100%");
-tl.fromTo(".img1", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img2", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img2", { x: -300 }, { x: -300, delay: 5 }, "100%");
+tl.fromTo(".img2", { x: 0 }, { x: -300, delay: 2 }, "-50%");
 
-tl.fromTo(".img2", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img3", { x: 0 }, { x: -300 }, "100%");
-tl.fromTo(".img3", { x: 0 }, { x: -600, delay: 6 }, "100%");
+tl.fromTo(".img2", { x: -300 }, { x: -600, duration: 2 }, "-50%");
+tl.fromTo(".img3", { x: -300 }, { x: -600, duration: 2 }, "-50%");
+tl.fromTo(".img1", { x: 0 }, { x: 0, delay: 2 });
+tl.fromTo(".img1", { x: 0 }, { x: -300 }, "-50%");
+
+/*Img 2 &3 are ok */
+/*
+let tl = new TimelineMax({ repeat: -1 });
+
+tl.fromTo(".img2", { x: 0 }, { x: -300, delay: 2 }, "-50%");
+
+tl.fromTo(".img2", { x: -300 }, { x: -600, duration: 2 }, "-50%");
+tl.fromTo(".img3", { x: -300 }, { x: -600, duration: 2 }, "-50%");
+tl.fromTo(".img1", { x: 0 }, { x: 0, delay: 2 });
+tl.fromTo(".img1", { x: 0 }, { x: -300 }, "-50%");
+/*
+
+tl.fromTo(".img2", { x: 0 }, { x: -300 });
+tl.fromTo(".img2", { x: -300 }, { x: -300 });
+tl.fromTo(".img3", { x: 0 }, { x: -300 });
+tl.fromTo(".img3", { x: 0 }, { x: -600 });
+*/
