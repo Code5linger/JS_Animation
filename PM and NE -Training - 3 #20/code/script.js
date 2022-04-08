efdd58daf530634f720e858bf4c294d8bf445931
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-08 12:40:58 +0600

    W

--- PM and NE -Training - 3 #20/code/script.js
+++ PM and NE -Training - 3 #20/code/script.js
@@ -74,3 +74,24 @@ function crossfade() {
 
 // start the crossfade after next = 3 sec
 gsap.delayedCall(next, crossfade);
+
+/*Footnote */
+var tlmenu = new TimelineMax({ paused: true });
+
+tlmenu
+  .to("nav", 0.3, { autoAlpha: 1 })
+  .staggerFromTo(
+    "nav li",
+    0.5,
+    { y: 100, opacity: 0 },
+    { y: 0, opacity: 1 },
+    0.1
+  );
+
+$(".burgerIcon").click(function () {
+  tlmenu.play(0);
+});
+
+$(".closeButton").click(function () {
+  tlmenu.reverse(0);
+});
---
+++ PM and NE -Training - 3 #20/footnote/script.js
@@ -0,0 +1,19 @@
+var tlmenu = new TimelineMax({ paused: true });
+
+tlmenu
+  .to("nav", 0.3, { autoAlpha: 1 })
+  .staggerFromTo(
+    "nav li",
+    0.5,
+    { y: 100, opacity: 0 },
+    { y: 0, opacity: 1 },
+    0.1
+  );
+
+$(".burgerIcon").click(function () {
+  tlmenu.play(0);
+});
+
+$(".closeButton").click(function () {
+  tlmenu.reverse(0);
+});
