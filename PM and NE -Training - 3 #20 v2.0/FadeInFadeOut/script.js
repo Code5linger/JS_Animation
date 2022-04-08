766de21e55e00bafcaa2515c37c244906db4667e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-08 18:25:25 +0600

    Animation Fixed

--- PM and NE -Training - 3 #20 v2.0/FadeInFadeOut/script.js
+++ PM and NE -Training - 3 #20 v2.0/FadeInFadeOut/script.js
@@ -1,15 +1,15 @@
 const tl = gsap
   .timeline({
-    defaults: { duration: 1 },
+    defaults: { duration: 0.5, repeat: -1 },
   })
   .to("#divtest", { opacity: 0 })
+  .set("#divtest", { innerText: "A" })
+  .to("#divtest", { opacity: 1, duration: 2 }) /*, "+=2"*/
+  .to("#divtest", { opacity: 0 })
   .set("#divtest", { innerText: "B" })
-  .to("#divtest", { opacity: 1 }, "+=2")
+  .to("#divtest", { opacity: 1, duration: 3 }) /*, "+=2"*/
   .to("#divtest", { opacity: 0 })
   .set("#divtest", { innerText: "C" })
-  .to("#divtest", { opacity: 1 }, "+=2");
-function doFade() {
-  tl.restart();
-}
+  .to("#divtest", { opacity: 1, duration: 4 }); /*, "+=2"*/
 
 document.querySelector("load").addEventListener("load", doFade);
